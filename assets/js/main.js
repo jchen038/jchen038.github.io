93cc0f2a46cd6ff2e17bcaccb31885d2f7caea80
jchen038 <mfwinds@gmail.com>
2015-09-12 17:52:07 -0700

    photography page update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,10 @@
-(function() {
-  $('#tabs').tab();
+$(document).ready(function() {
+  var imageList = ['IMG_0332', 'IMG_0344', 'IMG_0383', 'IMG_1978',
+                    'IMG_2289', 'IMG_2392', 'IMG_5349', 'IMG_6254',
+                    'IMG_7634', 'IMG_8126'];
+  $.each(imageList, function(index, value) {
+    $("#photography").append("<button class='image-small' data-toggle='modal' data-target='#" + value + "'><img class='image-small' src='/assets/image/" + value + ".jpg'></button>");
+    $("#photography").append("<div class='modal fade' id='" + value + "' tabindex='-1' role='dialog'><div class='modal-dialog'><div class='modal-content'><img class='image-large' src='/assets/image/" + value + ".jpg'></div></div></img></div>");
+    $("#modal" + value).focus();
+  });
 });
